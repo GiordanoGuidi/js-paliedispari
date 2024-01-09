bf28bd548e5f4655cc4d201b4d80b5d0e524473b
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-09 15:48:37 +0100

    added prompt and validation

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-console.log('HELLO WORLD')
+// CHIEDO UNA PAROLA ALL'UTENTE CON IL PROPMPT
+const word = prompt('Dimmi una parola', 'anna').trim();
+console.log(word, word.length);
+
+
+//!VALIDAZIONE
+if(!isNaN(word) || !word || word.length < 2 ){
+    alert('Devi inserire una parola');
+}
