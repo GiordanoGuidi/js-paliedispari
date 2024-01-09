a1c554de40753ee515af132307565711778c13be
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-09 16:08:09 +0100

    added function with for cicle

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,21 @@
 // CHIEDO UNA PAROLA ALL'UTENTE CON IL PROPMPT
-const word = prompt('Dimmi una parola', 'anna').trim();
+const word = prompt('Dimmi una parola', 'marco').trim();
 console.log(word, word.length);
 
 
 //!VALIDAZIONE
 if(!isNaN(word) || !word || word.length < 2 ){
     alert('Devi inserire una parola');
+}else{
+    //DICHIARAZIONE FUNZIONE
+    function reverseWord(word){
+        //CICLO FOR PER SCOMPORRE LA PAROLA AL CONTRARIO
+        for(let i = word.length -1; i >= 0; i--){
+            console.log(word[i])
+        }
+
+        return message = `La parola .. è palindroma`
+    }
 }
+
+reverseWord(word);
