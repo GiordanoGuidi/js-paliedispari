b6f7a3cfc779aecbcf566ee8d310826256eb20f7
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-19 19:35:41 +0100

    modified layout

--- JS/script.js
+++ JS/script.js
@@ -29,26 +29,35 @@
 
 //#PARI E DISPARI
 // CHIEDO DI SCEGLIERE UN  NUMERO DA 1 A 5 E SE PARI O DISPARI
-const userNumber = parseInt(prompt('Dimmi un numero da 1 a 5', 4));
+// const userNumber = parseInt(prompt('Dimmi un numero da 1 a 5', 4));
 console.log('numero utente', userNumber);
+
 //! VALIDAZIONE
 if(isNaN(userNumber)|| userNumber < 1|| userNumber > 5){
     alert('Attenzione devi inserire un numero maggiore di 1 e minore di 5');
 }else{
     const userChoice = prompt('Scegli pari o dispari', 'pari').trim();
     console.log('SCELTA', userChoice);
+
     //! VALIDAZIONE
     if(userChoice !== 'pari' && userChoice !== 'dispari'){
         alert('Attenzione devi scegliere o pari o dispari');
     }else{
+        /**
+         * Generates a random number between min and max(included or not)
+         * @param {number} min Minimum number to be randomize
+         * @param {number} max Maximum number to be randomize
+         * @param {boolean} isMaxIncluded Whether the maximum should be included
+         * @returns The randomized number
+         */
         // FUNZIONE CHE GENERA UN NUMERO RANDOM DA 1 A 5
-        function getRandomNumber(){
-           let randomNumber = Math.floor(Math.random() * 5) + 1;
+        function getRandomNumber(min,max,isMaxIncluded){
+           let randomNumber = Math.floor(Math.random() * max - min ) + min;
            console.log('numero random', randomNumber);
            return randomNumber;
         }
         // SOMMO I DUE NUMERI
-        let sum = getRandomNumber() + userNumber;
+        let sum = getRandomNumber(1,5,true) + userNumber;
         console.log('SOMMA', sum);
         let message;
         // FUNZIONE CHE STABILISCE SE E' PARI O DISPARI
@@ -67,5 +76,4 @@ if(isNaN(userNumber)|| userNumber < 1|| userNumber > 5){
         isEvenOrOdd(sum);
         console.log('sono il messaggio ---', message);
     }
-
     }
