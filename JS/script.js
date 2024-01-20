eaeaeda5281f971ed37786489d84ccb3dd29df63
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-20 17:36:11 +0100

    added winner condition and validation

--- JS/script.js
+++ JS/script.js
@@ -28,52 +28,64 @@
 // reverseWord(word);
 
 //#PARI E DISPARI
-// CHIEDO DI SCEGLIERE UN  NUMERO DA 1 A 5 E SE PARI O DISPARI
-// const userNumber = parseInt(prompt('Dimmi un numero da 1 a 5', 4));
-console.log('numero utente', userNumber);
+    /**
+     * Generates a random number between min and max(included or not)
+     * @param {number} min Minimum number to be randomize
+     * @param {number} max Maximum number to be randomize
+     * @returns The randomized number
+     */
 
-//! VALIDAZIONE
-if(isNaN(userNumber)|| userNumber < 1|| userNumber > 5){
-    alert('Attenzione devi inserire un numero maggiore di 1 e minore di 5');
-}else{
-    const userChoice = prompt('Scegli pari o dispari', 'pari').trim();
-    console.log('SCELTA', userChoice);
+//#FUNZIONI
+// FUNZIONE CHE GENERA UN NUMERO RANDOM DA 1 A 5
+const getRandomNumber = (min,max)=>{
+   let randomNumber = Math.floor(Math.random() * max - min +min ) + min;
+   return randomNumber;
+}
+// FUNZIONE CHE DETERMINA SE LA SOMMA è PARI O DISPARI
+const isEven = (num)=>{
+    return (num % 2 === 0);
+}
 
-    //! VALIDAZIONE
-    if(userChoice !== 'pari' && userChoice !== 'dispari'){
-        alert('Attenzione devi scegliere o pari o dispari');
+// FUNZIONE CHE TRASFORMA LA PRIMA LETTERE DI UNA STRINGA IN MAIUSCOLA
+const capitalizeFirstLetter = (stringa)=>
+stringa.charAt(0).toUpperCase() + stringa.slice(1);
+
+
+//#RECUPERO GLI ELEMENTI    
+const formElement = document.querySelector('form');
+const inputElement = document.querySelector('input');
+const selectElement = document.querySelector('select');
+console.log(formElement,inputElement,selectElement)
+const messageElement = document.getElementById('message');
+//CREO ADD EVENT LISTNER SUL FORM
+formElement.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    //RECUPERO VALORE DELL'INPUT
+    const inputValue = parseInt(inputElement.value);
+    console.log('valore input', inputValue)
+    if(inputValue < 1 || inputValue > 5 || isNaN(inputValue)){
+        alert ('Devi inserire un numero compreso tra 1 e 5');
     }else{
-        /**
-         * Generates a random number between min and max(included or not)
-         * @param {number} min Minimum number to be randomize
-         * @param {number} max Maximum number to be randomize
-         * @param {boolean} isMaxIncluded Whether the maximum should be included
-         * @returns The randomized number
-         */
-        // FUNZIONE CHE GENERA UN NUMERO RANDOM DA 1 A 5
-        function getRandomNumber(min,max,isMaxIncluded){
-           let randomNumber = Math.floor(Math.random() * max - min ) + min;
-           console.log('numero random', randomNumber);
-           return randomNumber;
+        //RECUPERO VALORE DELLA SELECT
+        const userChoice = selectElement.value;
+        console.log('valore select', userChoice)
+        //INVOCO LA FUNZIONE CHE GENERA IL NUMERO RANDOM
+        const randomNumber = getRandomNumber(1,5);
+        console.log('numero random', randomNumber)
+        // SOMMO IL NUMERO DELL'UTENTE E IL RANDOM NUMBER
+        const sum = randomNumber + inputValue;
+        console.log('sono la somma', sum)
+        // dichiaro la funzione che determina se la somma è pari o dispari
+        const rightChoice = isEven(sum) ? 'pari' : 'dispari'
+        console.log(rightChoice);
+        console.log('sono da capitalizzare',userChoice);
+        const capitalizedUserChoice =  capitalizeFirstLetter(userChoice);
+        //CREO UNA CONDIZIONE PER DICHIARARE CHI HA VINTO
+        if(userChoice === rightChoice){
+            messageElement.innerText = `Hai scelto ${capitalizedUserChoice} e il numero è ${sum}. Hai vinto!`;
+        }else{
+            messageElement.innerText = `Hai scelto ${capitalizedUserChoice} e il numero è ${sum}. Hai perso!`;
         }
-        // SOMMO I DUE NUMERI
-        let sum = getRandomNumber(1,5,true) + userNumber;
-        console.log('SOMMA', sum);
-        let message;
-        // FUNZIONE CHE STABILISCE SE E' PARI O DISPARI
-        function isEvenOrOdd(number){
-            if(number % 2 === 0 && userChoice === 'pari'){
-                message = 'il numero è pari ha vinto l\'utente';
-            }else if(number % 2 !== 0 && userChoice === 'dispari'){
-                message = 'il numero è dispari a vinto l\'utente';
-            }else if(number % 2 === 0 && userChoice === 'dispari'){
-                message = 'il numero è pari ha vinto il computer'
-            }else if(number % 2 !== 0 && userChoice === 'pari'){
-                message = 'Il numero è dispari ha vinto il computer';
-            }
-            return message;
-        }
-        isEvenOrOdd(sum);
-        console.log('sono il messaggio ---', message);
+        formElement.classList.add('d-none');
     }
-    }
+});
